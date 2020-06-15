36bf16f5d9a8a4f169520de88bf27100983cd89a
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-06-15 19:59:57 -0400

    initialized

---
+++ jeffreypoe_lab02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Jeffrey Poe 
+//June 15th 2020
+//COP 2220 
+//Lab 02 
+int main(){
+
+int num1, num2, num3;
+  //Initializes variables
+  printf("Please enter 3 Numbers \n");
+  scanf("%d", &num1);
+  scanf("%d", &num2);
+  scanf("%d", &num3);
+ 
+ int average = num1+num2+num3 / 3;
+ printf("%i", average);
+  
+
+};s
