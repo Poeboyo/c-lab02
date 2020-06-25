c52e43e36e23eb76e5ecaae0d64d2f3ceb723ed0
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-06-24 22:33:37 -0400

    Did exercises

--- jeffreypoe_lab02.c
+++ jeffreypoe_lab02.c
@@ -6,6 +6,44 @@
 //June 24th 2020
 //COP 2220 
 //Lab 03 
+
+
+//ORIGINAL CODE 
+// a) if age >= 65 ;
+// printf( "Age is greater than or equal to 65\n" );
+// else
+// printf( "Age is less than 65\n" );
+// b) if (deduct < balance);
+// balance = balance - deduct;
+// printf("New balance is %.2f\n", balance);
+// else;
+// printf("Deduction of %.2f refused.\n", deduct);
+// printf("Would overdraw account.\n");
+// printf("Deduction = %.2f Final balance = %.2f",deduct, balance);
+//=====================================================================
+
+
+
+//REWORKED
+//a) if(age >= 65){
+// printf("Age is greater than or equal to 65\n")}
+// else{  printf( "Age is less than 65\n" )};
+
+// b) if (deduct < balance){
+// balance = balance - deduct
+// printf("New balance is %.2f\n", balance);}
+// else{
+// printf("Deduction of %.2f refused.\n", deduct);
+// printf("Would overdraw account.\n");
+// printf("Deduction = %.2f Final balance = %.2f",deduct, balance);};
+
+
+
+//======================================================================
+
+
+
+
 void absol(num1, num2){
     printf("Please enter a Number \n");
     scanf("%d", &num1);
