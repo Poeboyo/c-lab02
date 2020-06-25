0b71ef7285785e4caff32353da3e1d212a647010
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-06-24 22:24:02 -0400

    Lab 3 complete

--- jeffreypoe_lab02.c
+++ jeffreypoe_lab02.c
@@ -3,27 +3,20 @@
 #include <math.h>
 
 //Jeffrey Poe 
-//June 15th 2020
+//June 24th 2020
 //COP 2220 
-//Lab 02 
-float avg(int num1,int num2,int num3);
+//Lab 03 
+void absol(num1, num2){
+    printf("Please enter a Number");
+    scanf("%d", &num1);
+    printf("Please enter another number");
+    scanf("%d", &num2);
 
+    int absoluteVal = (((num1 - num2)*-1)*1);
+    printf("Your absolute value after subtraction is %i !", absoluteVal);
+};
  
 int main(){
-float response = avg(2,2,3);
-
 };
 
-float avg(int num1,int num2,int num3){
-   //Initializes variables
-  printf("Please enter 3 Numbers \n");
-  scanf("%d", &num1);
-  scanf("%d", &num2);
-  scanf("%d", &num3);
-  int sum = num1+num2+num3;
-  float average = sum/3.0;
- 
- return printf("Average of Selected Numbers %f", average); 
-
-};
 
